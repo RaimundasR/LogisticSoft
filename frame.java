b2812ee4f1a76dd9762897b39885bd3e91ede41c
RaimundasR <35451662+RaimundasR@users.noreply.github.com>
2018-09-24 21:03:52 +0300

    Add files via upload
    latest update, added there validation of fields

--- frame.java
+++ frame.java
@@ -11,17 +11,20 @@ import java.awt.event.MouseEvent;
 
 public class frame {
 
+
+
     public void  Window (){
 
 
         JTable table = new JTable();
         Object [] columns = {"Name","Ref_number","Barcode_number"};
         DefaultTableModel model = new DefaultTableModel();
+
         model.setColumnIdentifiers(columns);
         table.setModel(model);
 
-        table.setBackground(Color.blue);
-        table.setForeground(Color.white);
+        table.setBackground(Color.WHITE);
+        table.setForeground(Color.BLACK);
 
         Font font = new Font(Font.SERIF,Font.BOLD,15);
         table.setFont(font);
@@ -60,17 +63,39 @@ public class frame {
         JFrame Message = new JFrame();
 
 
+
         Object [] row = new Object[3];
+
         Add.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                row [0] = field1.getText();
-                row [1] = field2.getText();
-                row [2] = field3.getText();
 
-                model.addRow(row);
 
-            }
+                row[0] = field1.getText();
+                row[1] = field2.getText();
+                row[2] = field3.getText();
+
+
+                if (field1.getText().trim().isEmpty()) {
+
+                    JOptionPane.showMessageDialog(Message, "Please enter item name", "ERROR MESSAGE", JOptionPane.INFORMATION_MESSAGE);
+                }
+
+                else if (field2.getText().trim().isEmpty()){
+
+                    JOptionPane.showMessageDialog(Message,"Please reference number","ERROR MESSAGE",JOptionPane.INFORMATION_MESSAGE);
+                }
+                else if (field3.getText().trim().isEmpty()){
+
+                    JOptionPane.showMessageDialog(Message,"Please type or scan barcode","ERROR MESSAGE",JOptionPane.INFORMATION_MESSAGE);
+                }
+
+
+                    model.addRow(row);
+
+
+}
+
         });
 
         Clear.addActionListener(new ActionListener() {
@@ -85,7 +110,7 @@ public class frame {
                 }
                 else {
 
-                    System.out.println("Delete error");
+                    JOptionPane.showMessageDialog(Message,"Please select item to clear","ERROR MESSAGE",JOptionPane.INFORMATION_MESSAGE);
                 }
             }
         });
@@ -148,5 +173,6 @@ public class frame {
 
 
     }
+
 
 }
